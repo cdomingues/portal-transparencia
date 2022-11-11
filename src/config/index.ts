2be76f1601ac93569fa397f859b30642805c6412
Vinicius Oliveira <viniciusoliveirabranco@hotmail.com>
2022-11-11 02:01:17 -0300

    feat: change config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,1 @@
-export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://dadosabertos.mogidascruzes.sp.gov.br"
-    : "http://localhost:3000";
+export const baseUrl = "http://localhost:3000"
