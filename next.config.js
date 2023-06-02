e44e0cc362e1ee6d44aca7024b7f8ba586f4ddd8
sguirodrigues <samuel.rodrigues@capitual.com>
2023-06-02 01:15:52 -0300

    remove rewrites

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://licitacao-mgcon.mogidascruzes.sp.gov.br/arquivo/:path*',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
