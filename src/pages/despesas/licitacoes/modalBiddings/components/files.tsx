3e06ee84f9412ecb63856c8bcfc208e3b69cb4e1
ligapmmc <marcos.tcs@mogidascruzes.sp.gov.br>
2023-06-24 20:57:30 -0300

    baseUrl>>files.tsx

--- src/pages/despesas/licitacoes/modalBiddings/components/files.tsx
+++ src/pages/despesas/licitacoes/modalBiddings/components/files.tsx
@@ -1,15 +1,17 @@
 import { Table, TableContainer, Tbody, Td, Thead, Tr } from "@chakra-ui/react";
 import { TColumn } from "../../../../../styles/components/licitacoes/modal/styles";
 import axios from "axios";
+import { baseUrl } from "../../../../../config";
 
 const Files = ({ details }: any) => {
   const handleDownload = async (value: string) => {
     const splited = value?.split('/');
     const fileName = splited[splited?.length - 1];
   
-    const { data } = await axios.get(`/api/download/${fileName}`, {
+    const { data } = await axios.get(`${baseUrl}/api/download/${fileName}`, {
       responseType: 'arraybuffer',
     });
+     
   
     const blob = new Blob([data], { type: 'application/pdf' });
     const url = window.URL.createObjectURL(blob);
