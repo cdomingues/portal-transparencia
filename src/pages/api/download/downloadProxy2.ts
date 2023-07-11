05fc69be532023fb83e24db214bb021955727f92
ligapmmc <marcos.tcs@mogidascruzes.sp.gov.br>
2023-07-11 16:18:50 -0300

    Ajuste proxy2

---
+++ src/pages/api/download/downloadProxy2.ts
@@ -0,0 +1,65 @@
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import fs from 'fs';
+// import path from 'path';
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method === 'GET') {
+//     const { filename } = req.query;
+
+//     const filePath = path.join(process.cwd(), 'data', filename as string);
+
+//     try {
+//       if (fs.existsSync(filePath)) {
+//         const file = fs.readFileSync(filePath);
+//         const fileBase64 = Buffer.from(file).toString('base64');
+
+//         res.status(200).json({ fileBase64 });
+//       } else {
+//         res.status(404).json({ error: 'File not found.' });
+//       }
+//     } catch (err) {
+//       res.status(500).json({ error: 'Error reading file.' });
+//     }
+//   } else {
+//     res.setHeader('Allow', 'GET');
+//     res.status(405).end('Method Not Allowed');
+//   }
+// }
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
+import path from 'path';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const { filename } = req.query;
+
+    const filePath = path.join(process.cwd(), 'data', filename as string);
+
+    try {
+      if (fs.existsSync(filePath)) {
+        const file = fs.readFileSync(filePath);
+
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader(
+          'Content-Disposition',
+          'inline; filename=' + (filename as string)
+        );
+        res.status(200).send(file);
+      } else {
+        res.status(404).json({ error: 'File not found.' });
+      }
+    } catch (err) {
+      res.status(500).json({ error: 'Error reading file.' });
+    }
+  } else {
+    res.setHeader('Allow', 'GET');
+    res.status(405).end('Method Not Allowed');
+  }
+}
