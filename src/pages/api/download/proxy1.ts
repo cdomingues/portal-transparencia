66523904db45791c21aea54c59fdbf3dccd2fc49
ligapmmc <marcos.tcs@mogidascruzes.sp.gov.br>
2023-07-10 23:58:00 -0300

    Proxy1 ajuste

--- src/pages/api/download/proxy1.ts
+++ src/pages/api/download/proxy1.ts
@@ -1,3 +1,46 @@
+// import { NextApiRequest, NextApiResponse } from "next";
+// import axios from 'axios';
+// import fs from 'fs';
+// import path from 'path';
+// import url from 'url';
+
+// export default async function Proxy1(req: NextApiRequest, res: NextApiResponse) {
+//     if (req.method === 'POST') {
+//         const { link } = req.body;
+
+//         try {
+//             // Use Axios para fazer uma requisição GET ao link de download
+//             const response = await axios.get(link, { responseType: 'stream' });
+
+//             // Analise o link para obter o nome do arquivo e a extensão
+//             const parsedUrl = url.parse(link, true);
+//             const fileName = parsedUrl.query.arquivo as string;
+
+//             // Crie um stream de escrita para o arquivo de destino
+//             const downloadPath = path.resolve('./data', fileName);
+//             const writer = fs.createWriteStream(downloadPath);
+
+//             // Pipe o stream de leitura da resposta no stream de escrita do arquivo
+//             response.data.pipe(writer);
+
+//             writer.on('finish', () => {
+//                 res.json({ status: 'Download Completed' });
+//             });
+
+//             writer.on('error', (err) => {
+//                 console.error(err);
+//                 res.status(500).json({ status: 'Download Failed' });
+//             });
+//         } catch (err) {
+//             console.error(err);
+//             res.status(500).json({ status: 'Download Failed' });
+//         }
+//     } else {
+//         res.setHeader('Allow', 'POST');
+//         res.status(405).end('Method Not Allowed');
+//     }
+// }
+
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from 'axios';
 import fs from 'fs';
@@ -9,22 +52,22 @@ export default async function Proxy1(req: NextApiRequest, res: NextApiResponse) 
         const { link } = req.body;
 
         try {
-            // Use Axios para fazer uma requisição GET ao link de download
+            // Use Axios to make a GET request to the download link
             const response = await axios.get(link, { responseType: 'stream' });
 
-            // Analise o link para obter o nome do arquivo e a extensão
+            // Parse the link to get the file name and extension
             const parsedUrl = url.parse(link, true);
             const fileName = parsedUrl.query.arquivo as string;
 
-            // Crie um stream de escrita para o arquivo de destino
-            const downloadPath = path.resolve('./data', fileName);
+            // Create a write stream for the destination file
+            const downloadPath = path.resolve('./public/data', fileName);
             const writer = fs.createWriteStream(downloadPath);
 
-            // Pipe o stream de leitura da resposta no stream de escrita do arquivo
+            // Pipe the read stream from the response into the write stream of the file
             response.data.pipe(writer);
 
             writer.on('finish', () => {
-                res.json({ status: 'Download Completed' });
+                res.json({ status: 'Download Completed', downloadLink: `/data/${fileName}` });
             });
 
             writer.on('error', (err) => {
