cc944a53c2911095ab056dc8aa394d631520b4d7
ligapmmc <marcos.tcs@mogidascruzes.sp.gov.br>
2023-08-17 19:28:09 -0300

    Vlibras

--- src/components/Container/WithAside/index.tsx
+++ src/components/Container/WithAside/index.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import { Box, Stack } from "@chakra-ui/react";
 import { isMobile } from "react-device-detect";
 import { Container } from "./styles";
+import Vlibras from 'vlibras-nextjs';
 import { Size, useWindowSize } from "../../../hooks/useWindowSize";
 
 type PropsInput = {
@@ -22,7 +23,9 @@ function ContainerWithAside({ children, aSide }: PropsInput) {
 
   return (
     <Container>
+      <Vlibras/>
       <Stack direction="row" width={hasAside ? "100%" : "100%"}>
+
         <Stack flex={1}>{children}</Stack>
         <Stack flex={1}>{aSide}</Stack>
       </Stack>
