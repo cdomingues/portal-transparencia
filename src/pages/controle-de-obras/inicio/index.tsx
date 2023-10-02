d87c718679f43980ff81836513a84d8ae545fb78
ligapmmc <marcos.tcs@mogidascruzes.sp.gov.br>
2023-10-02 13:53:11 -0300

    ajuste obras

--- src/pages/controle-de-obras/inicio/index.tsx
+++ src/pages/controle-de-obras/inicio/index.tsx
@@ -20,27 +20,35 @@ import obras_mobilidade from "../../../assets/images/icones/Portal de Obras_TOPO
 import obras_saude from "../../../assets/images/icones/Portal de Obras_TOPO_saude.svg";
 import CardObras from "../../../components/CardObras";
 import imgBigTransformations from "../../../assets/images/big_transformations.png";
+import ContainerBasic from "../../../components/Container/Basic";
 
 type PropsInput = {
   handler?: {
     columns?: TableColumns;
     data?: Array<any>;
     loading?: boolean;
+    showAsideByDefault?: boolean; // Nova propriedade
+    showToggleButton?: boolean; // Isso irá omitir o botão
+    showFirstBox?: boolean; // Isso irá omitir o botão
   };
 };
 
 export const contentConstructionsControl = {
-  titlePage: "Portal de Obras - Mogi das Cruzes",
-  description: "Conheça as obras que estão transformando nossa cidade.",
+  titlePage: "Balanços Anuais",
+  description:
+    "O Balanço Anual mostra as principais ..",
 };
 
 function Screen({ handler }: PropsInput) {
   const router = useRouter();
   const accessibility = useFontSizeAccessibilityContext();
 
+  const title = contentConstructionsControl?.titlePage;
+  const description = contentConstructionsControl?.description;
+
   return (
-    <LayoutConstructions breadcrumb={false}>
-      <Style.Banner >
+    <ContainerBasic title={title} description={description} showAsideByDefault={false} showToggleButton={false} showFirstBox={false}>
+      <Style.Banner>
         <div className="content">
           <Heading
             color={colors.white}
@@ -57,12 +65,11 @@ function Screen({ handler }: PropsInput) {
           >
             {contentConstructionsControl?.description}
           </Text>
-          
-          <div className="banner-bottom" >
+
+          <div className="banner-bottom">
             <div
               className="chip-banner"
               onClick={() => router.push("/controle-de-obras/sobre-as-obras")}
-              
             >
               <AiFillInfoCircle fontSize={18} color={colors.white} />
 
@@ -70,7 +77,6 @@ function Screen({ handler }: PropsInput) {
                 color={colors.white}
                 fontSize={accessibility?.fonts?.medium}
                 fontWeight={700}
-                
               >
                 Sobre as obras
               </Text>
@@ -79,7 +85,6 @@ function Screen({ handler }: PropsInput) {
             <div
               className="chip-banner"
               onClick={() => router.push("/controle-de-obras/pesquisar-obras")}
-            
             >
               <AiOutlineSearch fontSize={18} color={colors.white} />
 
@@ -95,57 +100,58 @@ function Screen({ handler }: PropsInput) {
         </div>
         <Style.BannerTotals>
           <div className="gradient-image">
-          <div className="content">
-            {/* <CardTotal
+            <div className="content">
+              {/* <CardTotal
            // value="0"
             description={`Novas áreas de espaço público e áeras verdes`}
             icon={<TbUsers color={colors.white} fontSize={50} />}
           /> */}
 
-            <CardObras
-              //value="0"
-              description="Novas áreas de espaço público e áreas verdes"
-              // icon={obras_espaco_publico}
-              imageURL={obras_espaco_publico.src}
-            />
+              <CardObras
+                //value="0"
+                description="Novas áreas de espaço público e áreas verdes"
+                // icon={obras_espaco_publico}
+                imageURL={obras_espaco_publico.src}
+              />
 
-            <CardObras
-              //value="0"
-              description="Pessoas atendidas pelas obras de saneamento"
-              // icon={obras_espaco_publico}
-              imageURL={obras_saneamento.src}
-            />
+              <CardObras
+                //value="0"
+                description="Pessoas atendidas pelas obras de saneamento"
+                // icon={obras_espaco_publico}
+                imageURL={obras_saneamento.src}
+              />
 
-            <CardObras
-              //value="0"
-              description="Pessoas atendidas pelas obras de mobilidade"
-              // icon={obras_espaco_publico}
-              imageURL={obras_mobilidade.src}
-            />
+              <CardObras
+                //value="0"
+                description="Pessoas atendidas pelas obras de mobilidade"
+                // icon={obras_espaco_publico}
+                imageURL={obras_mobilidade.src}
+              />
 
-            <CardObras
-              //value="0"
-              description="Pessoas atendidas pelas obras de saúde"
-              // icon={obras_espaco_publico}
-              imageURL={obras_saude.src}
-            />
-          </div>
+              <CardObras
+                //value="0"
+                description="Pessoas atendidas pelas obras de saúde"
+                // icon={obras_espaco_publico}
+                imageURL={obras_saude.src}
+              />
+            </div>
           </div>
         </Style.BannerTotals>
       </Style.Banner>
 
       <MapOfConstructions />
 
-       <Style.ContainerBigTransformations>
+      <Style.ContainerBigTransformations>
         <Style.Heading>Grandes transformações da cidade</Style.Heading>
-        <div >
-        <a href="https://www.mogidascruzes.sp.gov.br/noticia/prefeitura-inaugura-o-vagalume-saude-infantil-neste-sabado-as-10-horas" target="_blank">
-          <img  src={imgBigTransformations.src} 
-          alt="" /> <label>Inauguraçao Hospital Vagalume</label></a>
-
+        <div>
+          <a
+            href="https://www.mogidascruzes.sp.gov.br/noticia/prefeitura-inaugura-o-vagalume-saude-infantil-neste-sabado-as-10-horas"
+            target="_blank"
+          >
+            <img src={imgBigTransformations.src} alt="" />{" "}
+            <label>Inauguraçao Hospital Vagalume</label>
+          </a>
         </div>
-        
-      
       </Style.ContainerBigTransformations>
 
       <Style.ContainerCardsTransformations>
@@ -174,7 +180,143 @@ function Screen({ handler }: PropsInput) {
           />
         </div>
       </Style.ContainerCardsTransformations>
-    </LayoutConstructions>
+    </ContainerBasic>
+
+    // <LayoutConstructions breadcrumb={false}>
+    //   <Style.Banner >
+    //     <div className="content">
+    //       <Heading
+    //         color={colors.white}
+    //         fontSize={accessibility?.fonts?.moreUltraLarge}
+    //         marginTop={50}
+    //       >
+    //         {contentConstructionsControl?.titlePage}
+    //       </Heading>
+
+    //       <Text
+    //         color={colors.white}
+    //         fontSize={accessibility?.fonts?.large}
+    //         marginTop={10}
+    //       >
+    //         {contentConstructionsControl?.description}
+    //       </Text>
+
+    //       <div className="banner-bottom" >
+    //         <div
+    //           className="chip-banner"
+    //           onClick={() => router.push("/controle-de-obras/sobre-as-obras")}
+
+    //         >
+    //           <AiFillInfoCircle fontSize={18} color={colors.white} />
+
+    //           <Text
+    //             color={colors.white}
+    //             fontSize={accessibility?.fonts?.medium}
+    //             fontWeight={700}
+
+    //           >
+    //             Sobre as obras
+    //           </Text>
+    //         </div>
+
+    //         <div
+    //           className="chip-banner"
+    //           onClick={() => router.push("/controle-de-obras/pesquisar-obras")}
+
+    //         >
+    //           <AiOutlineSearch fontSize={18} color={colors.white} />
+
+    //           <Text
+    //             color={colors.white}
+    //             fontSize={accessibility?.fonts?.medium}
+    //             fontWeight={700}
+    //           >
+    //             Pesquise obras
+    //           </Text>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <Style.BannerTotals>
+    //       <div className="gradient-image">
+    //       <div className="content">
+    //         {/* <CardTotal
+    //        // value="0"
+    //         description={`Novas áreas de espaço público e áeras verdes`}
+    //         icon={<TbUsers color={colors.white} fontSize={50} />}
+    //       /> */}
+
+    //         <CardObras
+    //           //value="0"
+    //           description="Novas áreas de espaço público e áreas verdes"
+    //           // icon={obras_espaco_publico}
+    //           imageURL={obras_espaco_publico.src}
+    //         />
+
+    //         <CardObras
+    //           //value="0"
+    //           description="Pessoas atendidas pelas obras de saneamento"
+    //           // icon={obras_espaco_publico}
+    //           imageURL={obras_saneamento.src}
+    //         />
+
+    //         <CardObras
+    //           //value="0"
+    //           description="Pessoas atendidas pelas obras de mobilidade"
+    //           // icon={obras_espaco_publico}
+    //           imageURL={obras_mobilidade.src}
+    //         />
+
+    //         <CardObras
+    //           //value="0"
+    //           description="Pessoas atendidas pelas obras de saúde"
+    //           // icon={obras_espaco_publico}
+    //           imageURL={obras_saude.src}
+    //         />
+    //       </div>
+    //       </div>
+    //     </Style.BannerTotals>
+    //   </Style.Banner>
+
+    //   <MapOfConstructions />
+
+    //    <Style.ContainerBigTransformations>
+    //     <Style.Heading>Grandes transformações da cidade</Style.Heading>
+    //     <div >
+    //     <a href="https://www.mogidascruzes.sp.gov.br/noticia/prefeitura-inaugura-o-vagalume-saude-infantil-neste-sabado-as-10-horas" target="_blank">
+    //       <img  src={imgBigTransformations.src}
+    //       alt="" /> <label>Inauguraçao Hospital Vagalume</label></a>
+
+    //     </div>
+
+    //   </Style.ContainerBigTransformations>
+
+    //   <Style.ContainerCardsTransformations>
+    //     <Style.Heading>Conheça nossas obras em destaque</Style.Heading>
+
+    //     <div className="align-cards">
+    //       <CardTransformation
+    //         title="Grandes transformações da cidade"
+    //         description="Temos um plano para continuar melhorando a qualidade de vida dos moradores da cidade."
+    //         backgroundImageSrc="https://dados.mogidascruzes.sp.gov.br/dataset/8e668745-1f91-4e64-a541-4f4a81898cac/resource/5c7bcad9-6a64-4fcf-8a83-89a617371b49/download/obras-01.png"
+    //         onClick={() =>
+    //           router.push("/controle-de-obras/grandes-transformacoes-da-cidade")
+    //         }
+    //       />
+    //       <CardTransformation
+    //         title="Transformações em Mogi das Cruzes"
+    //         description="Explore os bairros da cidade para descobrir o que estamos fazendo em cada um."
+    //         backgroundImageSrc="https://dados.mogidascruzes.sp.gov.br/dataset/8e668745-1f91-4e64-a541-4f4a81898cac/resource/23585d54-3214-4c8b-8474-97dc80ada803/download/obras-02.png"
+    //         onClick={() => router.push("/controle-de-obras/bairros")}
+    //       />
+    //       <CardTransformation
+    //         title="Espaço público de qualidade"
+    //         description="Trabalhamos por uma cidade com mais e melhor espaço público e áreas verdes, agradáveis, seguras e próximas aos moradores."
+    //         backgroundImageSrc="https://dados.mogidascruzes.sp.gov.br/dataset/8e668745-1f91-4e64-a541-4f4a81898cac/resource/4be78cfe-7506-4aeb-afb2-5be46d0d86fd/download/obras-03.png"
+    //         onClick={() => router.push("/controle-de-obras/espaco-publico")}
+    //       />
+    //     </div>
+    //   </Style.ContainerCardsTransformations>
+    // </LayoutConstructions>
   );
 }
 
