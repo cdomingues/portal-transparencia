3c2ce173522e06b5f042dcdcd8543d8c66285f38
ligapmmc <marcos.tcs@mogidascruzes.sp.gov.br>
2023-10-05 12:47:40 -0300

    ajuste noticias

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 // Production use this variable
-// export const baseUrl = "https://dadosabertos.mogidascruzes.sp.gov.br";
+export const baseUrl = "https://dadosabertos.mogidascruzes.sp.gov.br";
 
 // Development use this variable
-export const baseUrl = "http://localhost:3000";
+// export const baseUrl = "http://localhost:3000";
 
 export const revalidate = 3600 * 4;
