d3469266a892cf387b5145e3ce3356f35d1ab616
ligapmmc <marcos.tcs@mogidascruzes.sp.gov.br>
2023-10-11 14:02:54 -0300

    cwd

--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -9,6 +9,9 @@ fi
 # Salvar a configuração atual do PM2 antes de qualquer ação
 /usr/local/bin/pm2 save --force
 
+# Navegar até o diretório onde o conteúdo do site está localizado
+cd /var/www/dadosabertos/dadosabertos || { echo "Falha ao entrar no diretório do projeto. Saindo..."; exit 1; }
+
 # Iniciar o portal
 /usr/local/bin/pm2 start /usr/local/bin/yarn --name "dadosabertos" -- start
 
@@ -16,13 +19,13 @@ fi
 /usr/local/bin/yarn build && echo "Construção bem-sucedida" || { echo "Falha na construção"; exit 1; }
 
 # Reiniciar todos os processos
-/usr/local/bin/pm2 restart all && echo "Todos os processos reiniciados com sucesso" || { echo "Falha ao reiniciar processos"; exit 1; }
+/usr/local/bin/pm2 restart all --cwd /var/www/dadosabertos/dadosabertos && echo "Todos os processos reiniciados com sucesso" || { echo "Falha ao reiniciar processos"; exit 1; }
 
 # Reconstruir o projeto
 /usr/local/bin/yarn build && echo "Reconstrução bem-sucedida" || { echo "Falha na reconstrução"; exit 1; }
 
 # Reiniciar todos os processos novamente
-/usr/local/bin/pm2 restart all && echo "Todos os processos reiniciados com sucesso" || { echo "Falha ao reiniciar processos"; exit 1; }
+/usr/local/bin/pm2 restart all --cwd /var/www/dadosabertos/dadosabertos && echo "Todos os processos reiniciados com sucesso" || { echo "Falha ao reiniciar processos"; exit 1; }
 
 # Salvar a configuração atual do PM2 após todas as ações
 /usr/local/bin/pm2 save --force && echo "Configuração do PM2 salva com sucesso" || { echo "Falha ao salvar a configuração do PM2"; exit 1; }
