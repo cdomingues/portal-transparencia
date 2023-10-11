ebe83378a70ca28d010d5ea50e4998a5cf0342cc
ligapmmc <marcos.tcs@mogidascruzes.sp.gov.br>
2023-10-11 16:56:06 -0300

    ajuste restart

--- scripts/monitorar_portal.sh
+++ scripts/monitorar_portal.sh
@@ -6,17 +6,21 @@ URL="https://dadosabertos.mogidascruzes.sp.gov.br/"
 # Caminho para o script de reinicialização
 SCRIPT_REINICIO="/var/www/dadosabertos/dadosabertos/scripts/startServer.sh"
 
+# E-mail para receber notificações
+EMAIL="marcostorres@mogidascruzes.sp.gov.br"
+
 # Fazer uma requisição HTTP e capturar o código de status
 STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" -k $URL)
 
-# Verificar se o código de status é 504 ou 502
-if [ $STATUS -eq 504 ] || [ $STATUS -eq 502 ]; then
-  echo "Erro $STATUS detectado. Iniciando rotina de reinicialização."
+# Verificar se o código de status é diferente de 200
+if [ $STATUS -ne 200 ]; then
+  echo "Erro $STATUS detectado. Iniciando rotina de reinicialização." | mail -s "Alerta: Portal Fora do Ar" $EMAIL
 
   # Executar o script de reinicialização
   bash $SCRIPT_REINICIO
 
-  echo "Rotina de reinicialização concluída."
+  echo "Rotina de reinicialização concluída." | mail -s "Atualização: Rotina de Reinicialização Concluída" $EMAIL
 else
   echo "O portal está operacional."
 fi
+
