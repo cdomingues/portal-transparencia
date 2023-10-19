71861250b04ddb0f35c5cf725b17b3fbf11c1d73
Vinicius Oliveira <vinicius@invams.com>
2023-10-18 21:28:17 -0300

    feat: comment rewrites with redirect

--- next.config.js
+++ next.config.js
@@ -3,30 +3,34 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
 
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://licitacao-mgcon.mogidascruzes.sp.gov.br/arquivo/:path*',
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://licitacao-mgcon.mogidascruzes.sp.gov.br/arquivo/:path*',
+  //     },
+  //   ];
+  // },
 
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
         ],
       },
     ];
-  }
-
+  },
 };
 
 module.exports = nextConfig;
-
-
