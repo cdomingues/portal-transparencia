1d19c57ace3a714add0e762a374988a0401573b7
cdomingues <domingues_santos@hotmail.com>
2023-10-23 15:57:27 -0300

    atualização pagina LAI

--- src/pages/acesso-a-informacao/screen.tsx
+++ src/pages/acesso-a-informacao/screen.tsx
@@ -27,6 +27,7 @@ import {
   Button,
   Text,
   useColorModeValue,
+  textDecoration,
 } from "@chakra-ui/react";
 import moneyFormatter from "../../utils/moneyFormatter";
 import moment from "moment";
@@ -43,6 +44,8 @@ import noticias from '../../../data/noticias.json'
 import DisplayNews from "../../components/NewsHome";
 import ContainerBasic from "../../components/Container/Basic";
 import Link from "next/link";
+import router from "next/router";
+import colors from "../../styles/colors";
 
 
 
@@ -69,7 +72,7 @@ type PropsInput = {
 export const contentInitial = {
   titlePage: "LEI DE ACESSO À INFORMAÇÃO",
   description:
-    "A Lei nº 12.527, sancionada em 18 de novembro de 2011, regulamenta o direito constitucional de acesso dos cidadãos à informação e sua restrição. A LAI (Lei de Acesso à Informação) é um mecanismo que permite a qualquer cidadão receber informações de seu interesse particular ou de interesse coletivo ou geral, que serão prestadas no prazo da lei, sob pena de responsabilidade, ressalvadas aquelas cujo sigilo seja imprescindível à segurança da sociedade e do Estado.",
+    "A Lei Federal nº 12.527, sancionada em 18 de novembro de 2011, regulamenta o direito constitucional de acesso à informação, conhecida como LAI, Lei de Acesso à Informação ou Lei da Transparência, a lei é um mecanismo que permite a qualquer cidadão receber informações de seu interesse particular ou de interesse coletivo ou geral, que serão prestadas no prazo da lei, sob pena de responsabilidade, ressalvadas aquelas cujo sigilo seja imprescindível à segurança da sociedade, do Município ou do Estado.",
 };
 
  function Aside() {
@@ -160,41 +163,25 @@ overflow="hidden"
 maxWidth="100%"
 borderRadius="18px"
 marginBottom="15px">
+      
+      
+      
+      
+      
+      <Text
+       align={isMobile ? "justify" : "left"}
+       color="gray.500"
+       fontSize={accessibility?.fonts?.regular}>
+       Em Mogi das Cruzes a regulamentação local foi realizada pela: <Link  href="http://leismunicipa.is/0ucro" target="blank" style={{ color: "#db334f" }}>Lei Municipal nº 7.986, de 28 de setembro de 2023. </Link> 
+      
+      </Text>
+      
       <Text
                 align={isMobile ? "justify" : "left"}
                 fontWeight="700"
                 fontSize={accessibility?.fonts?.regular}
               >
-                Prazo:
-                
-                
-              </Text>
-        <Text
-                align={isMobile ? "justify" : "left"}
-                color="gray.500"
-                fontSize={accessibility?.fonts?.regular}
-              >20 (vinte) dias corridos, prorrogáveis por mais 10 (dez) dias, totalizando 30 (trinta) dias.</Text>
-
-<Text
-                align={isMobile ? "justify" : "left"}
-                fontWeight="700"
-                fontSize={accessibility?.fonts?.regular}
-              >
-                Recursos:
-                
-                
-              </Text>
-        <Text
-                align={isMobile ? "justify" : "left"}
-                color="gray.500"
-                fontSize={accessibility?.fonts?.regular}
-              >apresentados em até 10 (dez) dias, por meio da plataforma online ou presencial, contra a decisão proferida. </Text>
-              <Text
-                align={isMobile ? "justify" : "left"}
-                fontWeight="700"
-                fontSize={accessibility?.fonts?.regular}
-              >
-                Meios de solicitação: 
+                Como fazer uma Solicitação de acesso à informação - LAI (online e presencial) :
                 
                 
               </Text>
@@ -211,35 +198,7 @@ marginBottom="15px">
                 align={isMobile ? "justify" : "left"}
                 color="gray.500"
                 fontSize={accessibility?.fonts?.regular}
-              > <Link href='https://lai.mogidascruzes.sp.gov.br'>lai.mogidascruzes.sp.gov.br</Link></Text>
-              <Text
-                align={isMobile ? "justify" : "left"}
-                fontWeight="700"
-                fontSize={accessibility?.fonts?.regular}
-              >
-                Telefone:
-                
-                
-              </Text>
-        <Text
-                align={isMobile ? "justify" : "left"}
-                color="gray.500"
-                fontSize={accessibility?.fonts?.regular}
-              >(11) 4798 - 5159 </Text>
-              <Text
-                align={isMobile ? "justify" : "left"}
-                fontWeight="700"
-                fontSize={accessibility?.fonts?.regular}
-              >
-                E-mail: 
-                
-                
-              </Text>
-        <Text
-                align={isMobile ? "justify" : "left"}
-                color="gray.500"
-                fontSize={accessibility?.fonts?.regular}
-              >lai@mogidascruzes.sp.gov.br </Text>
+              > <Link href='https://lai.mogidascruzes.sp.gov.br' target="blank" style={{ color: "#db334f" }}>lai.mogidascruzes.sp.gov.br</Link></Text>
 
 <Text
                 align={isMobile ? "justify" : "left"}
@@ -270,6 +229,163 @@ marginBottom="15px">
                 color="gray.500"
                 fontSize={accessibility?.fonts?.regular}
               >Segunda a sexta-feira, das 8 às 17 horas, no PAC do prédio sede da Prefeitura (Av. Ver. Narciso Yague Guimarães, 277 - Centro Cívico) </Text>
+
+<Text
+                align={isMobile ? "justify" : "left"}
+                fontWeight="700"
+                fontSize={accessibility?.fonts?.regular}
+              >
+                Telefone:
+                
+                
+              </Text>
+        <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >(11) 4798 - 5159 </Text>
+              <Text
+                align={isMobile ? "justify" : "left"}
+                fontWeight="700"
+                fontSize={accessibility?.fonts?.regular}
+              >
+                E-mail: 
+                
+                
+              </Text>
+        <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >lai@mogidascruzes.sp.gov.br </Text>
+
+
+      <Text
+                align={isMobile ? "justify" : "left"}
+                fontWeight="700"
+                fontSize={accessibility?.fonts?.regular}
+              >
+                Quem pode solicitar:
+                
+                
+              </Text>
+        <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >Qualquer pessoa, natural ou jurídica, poderá formular pedido de acesso à informação. </Text>
+
+<Text
+                align={isMobile ? "justify" : "left"}
+                fontWeight="700"
+                fontSize={accessibility?.fonts?.regular}
+              >
+                Prazos:
+                
+                
+              </Text>
+        <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >20 (vinte) dias corridos, prorrogáveis por mais 10 (dez) dias, totalizando 30 (trinta) dias corridos. </Text>
+
+
+<Text
+                align={isMobile ? "justify" : "left"}
+                fontWeight="700"
+                fontSize={accessibility?.fonts?.regular}
+              >
+                Taxas:
+                
+                
+              </Text>
+        <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >A busca e o fornecimento da informação são gratuitos, ressalvada a cobrança do valor referente ao custo dos serviços e dos materiais utilizados, tais como reprodução de documentos, mídias digitais e postagem.  </Text>
+            
+              <Text
+                align={isMobile ? "justify" : "left"}
+                fontWeight="700"
+                fontSize={accessibility?.fonts?.regular}
+              >
+                Forma de acompanhamento:
+                
+                
+              </Text>
+        <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >Por meio do  <Link  href="https://mogidascruzes.1doc.com.br/b.php?pg=wp/detalhes&itd=3" target="blank" style={{ color: "#db334f" }}  >sistema eletrônico</Link> ou pelo protocolo enviado por e-mail. .  </Text>
+
+       <Text
+                align={isMobile ? "justify" : "left"}
+                fontWeight="700"
+                fontSize={accessibility?.fonts?.regular}
+              >
+                Observações:
+                
+                
+              </Text>
+        <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >Antes de fazer uma solicitação, verifique nossa seção de <Link href="https://www.mogidascruzes.sp.gov.br/pagina/gabinete/institucional" target="_blank" style={{ color: "#db334f" }}>Perguntas Frequentes</Link>.   </Text>      
+              
+              <Text
+                align={isMobile ? "justify" : "left"}
+                fontWeight="700"
+                fontSize={accessibility?.fonts?.regular}
+              >
+                Confira nossos materiais a respeito:
+                
+                
+              </Text>
+        <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >  
+              <Link href="https://dados.mogidascruzes.sp.gov.br/dataset/539c1429-98f9-48dd-ae35-c47651cda8ce/resource/d8b64da1-4d96-42de-9eb8-f00ee2979763/download/1-cartilha-digital-referente-a-lei-de-acesso-a-informacao-lai-versao-2022.pdf" target="_blank" style={{ color:"#db334f" }}> 1ª Cartilha digital referente à Lei de Acesso à Informação - LAI (versão 2022) </Link>
+              
+              
+              </Text>       
+
+              <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >  
+              
+              <Link href="https://dados.mogidascruzes.sp.gov.br/dataset/539c1429-98f9-48dd-ae35-c47651cda8ce/resource/c343217d-9dad-46a9-8cd5-bb11767bcc43/download/2-cartilha-digital-referente-a-lei-de-acesso-a-informacao-lai-versao-2023.pdf" target="_blank" style={{ color: "#db334f" }}>2ª Cartilha digital referente à Lei de Acesso à Informação - LAI (versão 2023) </Link>
+                            
+              </Text>   
+              <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >  
+             
+              <Link href="https://dados.mogidascruzes.sp.gov.br/dataset/539c1429-98f9-48dd-ae35-c47651cda8ce/resource/bfc4fd73-954c-43c5-afdc-4fde928aa88f/download/manual-como-protocolar-um-pedido-de-acesso-a-informacao-online-lai.pdf" target="_blank" style={{ color: "#db334f" }}>Manual Como protocolar um Pedido de Acesso à Informação Online - LAI </Link>
+              
+              
+              </Text>  
+
+               <Text
+                align={isMobile ? "justify" : "left"}
+                color="gray.500"
+                fontSize={accessibility?.fonts?.regular}
+              >  
+              
+              <Link href="https://dados.mogidascruzes.sp.gov.br/dataset/539c1429-98f9-48dd-ae35-c47651cda8ce/resource/65c33fa7-4ad2-4d5f-83cf-9078aa8bdbc5/download/relatorio-anual-lai-2022-transparencia-passiva.pdf" target="_blank" style={{ color: "#db334f" }}>Relatório Anual LAI 2022 - Transparência Passiva </Link>
+              
+              </Text>    
+
+
       </Box>
     <Stack
  style={{
@@ -313,7 +429,7 @@ marginBottom="15px"
               >
                  <Box padding="6" bg="transparent" flexDirection="row">
                   <CardHorizon
-                      title="Relatório de Demandas LAI"
+                      title="Relatório em tempo real de pedidos online"
                       imageURL={relatorio_de_demandas.src}
                       description="Pedido de e-SIC."
                       link="https://mogidascruzes.1doc.com.br/b.php?pg=o/transparencia"
@@ -322,7 +438,7 @@ marginBottom="15px"
                  
                   <Box padding="6" bg="transparent" flexDirection="row">
                     <CardHorizon
-                      title="Acesso a Informação LAI"
+                      title="Clique aqui e protocole seu pedido"
                       imageURL={acesso_a_informacao.src}
                       description="Abertura e Consulta de Pedidos de e-Sic"
                       link="https://mogidascruzes.1doc.com.br/b.php?pg=wp/detalhes&itd=3"
@@ -330,8 +446,31 @@ marginBottom="15px"
 
 
                   </Box>
+
+                  
                
               </Stack>
+
+              <Stack
+                direction={isMobile ? "column" : "row"}
+                align={isMobile ? "center" : "center"}
+                justifyContent={isMobile ? "flex-start" : "center"}
+              >
+                 <Box padding="6" bg="transparent" flexDirection="row">
+                  <CardHorizon
+                      title="Acompanhe os dados genéricos dos pedidos "
+                      imageURL={relatorio_de_demandas.src}
+                      description="Pedido de e-SIC."
+                      link="https://mogidascruzes.1doc.com.br/b.php?pg=wp/wp&itd=3&ss=5 "
+                      backgroundColor="transparent"/>
+                  </Box>
+                 
+                 
+
+                  
+               
+              </Stack>
+
               <Stack
                 direction={isMobile ? "column" : "row"}
                 align={isMobile ? "center" : "center"}
