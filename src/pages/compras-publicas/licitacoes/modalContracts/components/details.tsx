bf9927c606d8d54a177b48c43a0ea3af318d7d45
Carlos <domingues_santos@hotmail.com>
2024-08-20 16:55:43 -0300

    ajustes pagina lciitaçcoes

--- src/pages/compras-publicas/licitacoes/modalContracts/components/details.tsx
+++ src/pages/compras-publicas/licitacoes/modalContracts/components/details.tsx
@@ -1,59 +1,4 @@
 import { RowDetails } from "../../../../../styles/components/contratos-atas/modal/styles";
-const getSituacaoText = (situacao: string | undefined) => {
-  switch (situacao) {
-    case 'P':
-      return 'Publicada';
-    case 'S':
-      return 'Suspensa';
-    default:
-      return 'Desconhecida'; // Valor padrão para quando situacao não é reconhecida
-  }
-};
-
-const getTipoText = (tipo: number | undefined) => {
-  switch (tipo) {
-    case 2:
-      return 'Pregão Presencial';
-    case 3:
-      return 'Tomada de Preços';
-    case 4:
-      return 'Concorrência';
-    case 5:
-      return 'Leilão';
-    case 7:
-      return 'Chamada Pública';
-    case 8:
-      return 'Chamamento Público';
-    case 9:
-      return 'Pré-Qualificação';
-    case 10:
-      return 'Convite';
-    case 11:
-      return 'Dispensa/Inexigibilidade';
-    case 12:
-      return 'Chamamento Público - Saúde';
-    case 13:
-      return 'Chamamento Público - Esportes';
-    case 14:
-      return 'Chamamento Público - Verde e Meio Ambiente';
-    case 15:
-      return 'Chamada Pública - Saúde';
-    case 16:
-      return 'Chamamento Público - Educação';
-    case 17:
-      return 'Pregão Eletrônico';
-    case 18:
-      return 'Credenciamento';
-    case 20:
-      return 'Audiência Pública - Saúde';
-    case 21:
-      return 'CONCURSO';
-    case 27:
-      return 'PROCESSO SELETIVO';
-    default:
-      return 'Desconhecido'; // Valor padrão para quando tipo não é reconhecido
-  }
-};
 
 
 const Details = ({ contract }: any) => {
@@ -67,12 +12,12 @@ const Details = ({ contract }: any) => {
 
         <div className="column">
           <div className="title">Situação:</div>
-          <div className="value">{getSituacaoText(contract?.situacao)}</div>
+          <div className="value">{contract?.situacao}</div>
         </div>
 
         <div className="column">
           <div className="title">Tipo:</div>
-          <div className="value">{getTipoText(contract?.id_tipolicitacao)}</div>
+          <div className="value">{contract?.id_tipolicitacao}</div>
         </div>
 
        
