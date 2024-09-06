09f862fd32c7220727b1fcdcdd0927a3e56f2066
Carlos <domingues_santos@hotmail.com>
2024-09-06 13:33:06 -0300

    ajuste agenda

--- src/pages/agenda-aberta/agenda-chefe-gabinete/screen.tsx
+++ src/pages/agenda-aberta/agenda-chefe-gabinete/screen.tsx
@@ -18,8 +18,6 @@ import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { ptBR } from "date-fns/locale";
 import moment from "moment";
-import { baseUrl } from "../../../config";
-import { getScheduleMayor } from "../../../calls/agenda/agenda";
 import { useFontSizeAccessibilityContext } from "../../../context/fontSizeAccessibility";
 import { isMobile } from "react-device-detect";
 import Video from "../../../components/Videos";
@@ -54,61 +52,45 @@ function Screen({ handler }: PropsInput) {
   const [selected, setSelected] = useState<Date>();
   const [schedule, setSchedule] = useState<Array<Meeting>>([]);
 
-  
+  useEffect(() => {
+    // Função para buscar os dados das duas APIs e combinar os resultados
+    const fetchSchedules = async () => {
+      try {
+        const [data1, data2] = await Promise.all([
+          fetch('https://dadosadm.mogidascruzes.sp.gov.br/api/pessoas/99c579da-595e-439f-a105-e59ef3cd4e1a/').then(response => response.json()),
+          fetch('https://dadosadm.mogidascruzes.sp.gov.br/api/pessoas/d22ba2d3-d29f-4104-973c-341074bf1149/').then(response => response.json()),
+        ]);
 
-  /* const handleGetOpenSchedule = async () => {
-    const response = await fetch(
-      "https://dados.mogidascruzes.sp.gov.br/api/3/action/datastore_search?resource_id=e6ee12e9-2fec-4d91-acac-36b36bd179c2&q=Caio%20Cunha&limit=3000",
-      //"https://dadosadm.mogidascruzes.sp.gov.br/api/pessoas/d684362d-1a38-4b00-a4ab-11d3c7583af0/",
-      {
+        // Combinando os resultados das duas APIs
+        const combinedSchedule = [
+          ...data1.agenda_set,
+          ...data2.agenda_set,
+        ];
         
+        setSchedule(combinedSchedule);
+      } catch (error) {
+        console.error("Erro ao buscar os dados:", error);
       }
-    );
+    };
 
-    const data = await response.json();
-
-    if (!data) {
-      return;
-    }
-
-    return setSchedule(data?.result?.records  );
-  }; 
-
-  useEffect(() => {
-    handleGetOpenSchedule(); 
-  }, []);*/
-
-
-  useEffect(() => {
-    fetch('https://dadosadm.mogidascruzes.sp.gov.br/api/pessoas/99c579da-595e-439f-a105-e59ef3cd4e1a/') 
-       
-    .then(response =>response.json())
-    .then(data =>{
-      setSchedule(data.agenda_set)
-    })
+    fetchSchedules();
   }, []); 
 
   const filteredValues = schedule
-  ?.filter((item: Meeting) => {
-    // Linha modificada abaixo
-    const timeWithSubtraction = moment(item?.data_compromisso);
-    const isSameDate = timeWithSubtraction.format("YYYY-MM-DD") === String(moment(selected).format("YYYY-MM-DD"));
-    const isNotFutureDate = timeWithSubtraction.isSameOrBefore(moment(), 'day');
-    return  isSameDate && isNotFutureDate
-  })
-  // Uma linha depois para contexto
-  .sort((a: Meeting, b: Meeting) => {
-    // Linha modificada abaixo
-    const aHours = moment(a?.data_compromisso).format("HH:mm");
-    // Linha modificada abaixo
-    const bHours = moment(b?.data_compromisso).format("HH:mm");
-    return aHours > bHours ? 1 : -1;
-  });
+    ?.filter((item: Meeting) => {
+      const timeWithSubtraction = moment(item?.data_compromisso);
+      const isSameDate = timeWithSubtraction.format("YYYY-MM-DD") === String(moment(selected).format("YYYY-MM-DD"));
+      const isNotFutureDate = timeWithSubtraction.isSameOrBefore(moment(), 'day');
+      return isSameDate && isNotFutureDate;
+    })
+    .sort((a: Meeting, b: Meeting) => {
+      const aHours = moment(a?.data_compromisso).format("HH:mm");
+      const bHours = moment(b?.data_compromisso).format("HH:mm");
+      return aHours > bHours ? 1 : -1;
+    });
 
   const title = contentMayorAgenda?.titlePage;
   const description = contentMayorAgenda?.description;
-
-  
 
   const dateSelected = moment(selected).format("LL");
   const translatorMonth: any = {
@@ -138,7 +120,6 @@ function Screen({ handler }: PropsInput) {
     <ContainerBasic title={title} description={description}>
       <Video url_video={url_video} titulo={titulo} />
 
-
       <Box
         m={0}
         bg={useColorModeValue("white", "gray.800")}
@@ -159,12 +140,12 @@ function Screen({ handler }: PropsInput) {
           </Heading>
 
           <Text
-                          fontSize={accessibility?.fonts?.large}
-                          color="text.dark"
-                          style={{ margin: 0 }}
-                        >
-                          Fabiana Camacho Bava
-                        </Text>
+            fontSize={accessibility?.fonts?.large}
+            color="text.dark"
+            style={{ margin: 0 }}
+          >
+            Fabiana Camacho Bava
+          </Text>
 
           <Stack direction="row" flexWrap="wrap-reverse">
             <Stack
@@ -196,9 +177,8 @@ function Screen({ handler }: PropsInput) {
                   const timeWithSubtraction = moment(item?.data_compromisso);
                   const getHours = timeWithSubtraction.format("HH:mm").split(":");
 
-                  const timeWithSubtraction2 = moment(item?.data_compromisso);
+                  const timeWithSubtraction2 = moment(item?.fim_compromisso);
                   const getHours2 = timeWithSubtraction2.format("HH:mm").split(":");
-                  
 
                   return (
                     <>
@@ -209,7 +189,7 @@ function Screen({ handler }: PropsInput) {
                           color="red"
                           marginTop={2}
                         >
-                         Hora  Início: {getHours?.[0]}:{getHours?.[1]} {item?.fim_compromisso ? ` - Hora fim: ${getHours2?.[0]}:${getHours2?.[1]}` : ''}
+                         Hora Início: {getHours?.[0]}:{getHours?.[1]} {item?.fim_compromisso ? ` - Hora fim: ${getHours2?.[0]}:${getHours2?.[1]}` : ''}
                         </Heading>
                         
                         <Heading
@@ -226,27 +206,43 @@ function Screen({ handler }: PropsInput) {
                         >
                           {item?.local}
                         </Text>
+                        <Text
+                          fontSize={accessibility?.fonts?.medium}
+                          color="text.dark"
+                          style={{ margin: 0 }}
+                        >
+                          {item?.nome}
+                        </Text>
                         <>
-      {item?.detalhe ? (
-        <Accordion allowMultiple>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Heading mb={1} fontSize={accessibility?.fonts?.medium} color="red" marginTop={2}>
-                  Detalhes:
-                </Heading>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel>
-              <Text fontSize={accessibility?.fonts?.medium} color="text.dark" style={{ margin: 0 }}>
-                {item?.detalhe}
-              </Text>
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      ) : null}
-    </>
+                          {item?.detalhe ? (
+                            <Accordion allowMultiple>
+                              <AccordionItem>
+                                <h2>
+                                  <AccordionButton>
+                                    <Heading
+                                      mb={1}
+                                      fontSize={accessibility?.fonts?.medium}
+                                      color="red"
+                                      marginTop={2}
+                                    >
+                                      Detalhes:
+                                    </Heading>
+                                    <AccordionIcon />
+                                  </AccordionButton>
+                                </h2>
+                                <AccordionPanel>
+                                  <Text
+                                    fontSize={accessibility?.fonts?.medium}
+                                    color="text.dark"
+                                    style={{ margin: 0 }}
+                                  >
+                                    {item?.detalhe}
+                                  </Text>
+                                </AccordionPanel>
+                              </AccordionItem>
+                            </Accordion>
+                          ) : null}
+                        </>
                         <div style={{ marginTop: 8 }}></div>
                         <Divider
                           width="100%"
@@ -262,22 +258,21 @@ function Screen({ handler }: PropsInput) {
             <Stack
               minW={300}
               maxW={isMobile ? "90%" : "100%"}
-              
-              
               direction="column"
               backgroundColor={useColorModeValue("white", "gray.800")}
               borderRadius={10}
               boxShadow="0px 1px 2px rgba(0, 0, 0, 0.3),
             0px 1px 3px 1px rgba(0, 0, 0, 0.15)"
               maxH={350}
-              style={{ marginBottom: 30}}
+              style={{ marginBottom: 30 }}
             >
               <DayPicker
-              
                 mode="single"
                 selected={selected}
                 onSelect={setSelected}
+                footer={null}
                 locale={ptBR}
+                fromYear={2023}
               />
             </Stack>
           </Stack>
