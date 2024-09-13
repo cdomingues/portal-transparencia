621bbaf60ec6e959f483ebb2fafc3025e12bfe78
Carlos Domingues dos Santos <domingues_santos@hotmail.com>
2024-09-12 22:10:31 -0300

    Update index.tsx pop up

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -19,35 +19,7 @@ const ModalPopup: React.FC = () => {
   }, [onOpen]);
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent width={["full", "700px"]} height={["full", "400px"]}>
-        <ModalHeader>
-          Transição para o Novo Portal de Transparência em Andamento
-        </ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          Estamos atualmente em processo de transição para nosso novo Portal de
-          Transparência. <br />
-          Durante essa fase, você poderá experienciar algumas interrupções no
-          serviço ou lentidão na navegação.
-          <br /><br />
-          Agradecemos sua paciência e compreensão enquanto trabalhamos para
-          melhorar sua experiência.
-          <br />
-          <br />
-          Caso queira acessar o conteúdo do portal anterior, acesse o link{" "}
-          <Link
-            color="blue.500"
-            href="https://portaldatransparencia.mogidascruzes.sp.gov.br/"
-            isExternal
-          >
-            Portal versão anterior
-          </Link>
-          .
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+   <div></div>
   );
 };
 
