a35462c55b0a9b0aee07a4e929a711c5cd2fa80d
Carlos <domingues_santos@hotmail.com>
2024-12-13 14:53:10 -0300

    atualização vlibras

--- src/components/Layout/Public/index.tsx
+++ src/components/Layout/Public/index.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import publicRoutes from "../../../routes/public";
 import Footer from "./Footer";
 import SideBar from "./SideBar";
-import VLibras from "vlibras-nextjs";
+import VLibras from "@djpfs/react-vlibras";
 import ModalPopup from "../../Modal";
+import {useEffect} from 'react'
 
 type Props = {
   children?: React.ReactNode;
 };
 
 function PublicLayout({ children }: Props) {
+  
   return (
     <>
-       {/* <VLibras forceOnload />  */}
+        <VLibras forceOnload={true} />
       <SideBar routes={publicRoutes}>{children}</SideBar>
      
       <Footer />
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,6 @@
 declare module 'react-gtm-module';
 
 
+interface Window {
+    VLibras: any; // Aqui você pode usar um tipo mais específico se souber as propriedades de VLibras
+  }
