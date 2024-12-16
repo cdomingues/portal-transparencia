31251f636bc0032e2fec836e6b7b986d571a3f06
Carlos <domingues_santos@hotmail.com>
2024-12-16 18:16:38 -0300

    ajuste vlibras

--- src/components/Layout/Public/index.tsx
+++ src/components/Layout/Public/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import publicRoutes from "../../../routes/public";
 import Footer from "./Footer";
 import SideBar from "./SideBar";
-import VLibras from "@djpfs/react-vlibras";
+//import VLibras from "@djpfs/react-vlibras";
 import ModalPopup from "../../Modal";
 import {useEffect} from 'react'
 
@@ -14,7 +14,7 @@ function PublicLayout({ children }: Props) {
   
   return (
     <>
-        <VLibras forceOnload={true} />
+       {/*  <VLibras forceOnload={true} /> */}
       <SideBar routes={publicRoutes}>{children}</SideBar>
      
       <Footer />
