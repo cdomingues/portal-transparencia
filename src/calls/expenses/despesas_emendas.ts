e2c48d51dc263179ec6d22c87da89815526f60f4
Carlos <domingues_santos@hotmail.com>
2025-04-14 16:00:34 -0300

    ajustes

--- src/calls/expenses/despesas_emendas.ts
+++ src/calls/expenses/despesas_emendas.ts
@@ -7,23 +7,17 @@ import { objetos_licitacao } from "../../utils/objetos_licitacao";
 export const getDiarias = async (years?: number) => {
   try {
     let nextPage = "https://dadosadm.mogidascruzes.sp.gov.br/api/despesas";
-    let allResults: any[] = [];
-    let allYears: number[] = [];
+    const url = "https://dadosadm.mogidascruzes.sp.gov.br/api/despesas?exercicio_empenho=2025";
+    const response = await axios.get(url);
 
-    while (nextPage) {
-      const response = await axios.get(nextPage);
+    const { results } = response.data;
 
-      const { results, next } = response.data;
-
-      allResults = allResults.concat(results);
-
-      nextPage = next;
-    }
+     
 
     // Filtra os resultados onde o campo 'vinculo' começa com '08'
-    const filteredResults = allResults.filter((row) => row.vinculo && row.vinculo.startsWith("08"));
+    const filteredResults = results.filter((row: { vinculo: string; }) => row.vinculo && row.vinculo.startsWith("08"));
 
-    const mappingRows = filteredResults.map((row) => {
+    const mappingRows = filteredResults.map((row: any) => {
       return {
         ...row,
         vlr_empenho: "R$" + row.vlr_empenho,
