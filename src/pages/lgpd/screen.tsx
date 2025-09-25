d85c80c95c0b1a4ec0e9095e87b20866f1d56f07
Carlos Domingues <carlosdomingues@mogidascruzes.sp.gov.br>
2025-09-25 09:45:32 -0300

    ajuste arquivo lgpd

--- src/pages/lgpd/screen.tsx
+++ src/pages/lgpd/screen.tsx
@@ -365,7 +365,7 @@ Sede da Prefeitura de Mogi das Cruzes - 3º andar
                 align={isMobile ? "justify" : "left"}
                 color="gray.500"
                 fontSize={accessibility?.fonts?.regular}
-              ><Link href="https://dadosadm.mogidascruzes.sp.gov.br/media/arquivos/6383a4fc-1a82-4ffb-a0e0-71dd3bdb29f0/Plano_de_Adequa%C3%A7%C3%A3o_LGPD_2025_final.pdf" target="blank">Plano de Adequação LGPD 2025</Link>   </Text>
+              ><Link href="https://dadosadm.mogidascruzes.sp.gov.br/media/arquivos/6383a4fc-1a82-4ffb-a0e0-71dd3bdb29f0/Plnao_de_Adequa%C3%A7%C3%A3o_LGPD_2025_2026_final_g2SqI3p.pdf" target="blank">Plano de Adequação LGPD 2025</Link>   </Text>
  
                 </Flex>
               </AccordionPanel>
