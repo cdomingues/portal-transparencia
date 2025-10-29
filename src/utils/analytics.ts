f21a60b3ef64bf6ed32a3dbd635c5e6f1c42c8ee
Carlos Domingues <carlosdomingues@mogidascruzes.sp.gov.br>
2025-10-29 16:43:56 -0300

    ajuste analitics

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,13 +1,4 @@
 import { usePathname, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
-import ReactGA from 'react-ga4'
-
-
-export const initGA = ()=> {
-    ReactGA.initialize('G-7XQWR7YJLF')
-}
-
-export const logPageView = (path: string) =>{
-    ReactGA.send({hitType: "pageview", page: path})
-}
+//import ReactGA from 'react-ga4'
 
