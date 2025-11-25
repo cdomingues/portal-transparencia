e81fa7a73a336ecea7a45a6fb12933cfaa8f9cf3
Carlos Domingues <carlosdomingues@mogidascruzes.sp.gov.br>
2025-11-25 16:37:48 -0300

    ajuste home

--- src/components/Despesas/index.tsx
+++ src/components/Despesas/index.tsx
@@ -304,6 +304,8 @@ function Despesas() {
             <option value="2024">2024</option>
             <option value="2023">2023</option>
             <option value="2022">2022</option>
+            <option value="2022">2021</option>
+            <option value="2022">2020</option>
           </select>
         </label>
 
