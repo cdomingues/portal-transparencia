8b7fe4425562b28977b132f50212b6f276ca3a22
Carlos Domingues <carlosdomingues@mogidascruzes.sp.gov.br>
2026-03-12 14:38:47 -0300

    att pag saldo de cargos

--- src/pages/gestao-de-pessoas/saldo-cargos/index.tsx
+++ src/pages/gestao-de-pessoas/saldo-cargos/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Screen from "./screen";
-import { tabela_saldo_cargos } from "../../../utils/tabela_saldo_cargos";
+import { saldo_cargos } from "../../../utils/tabela_saldo_cargos";
 
 function Controller({}: any) {
 
@@ -26,7 +26,7 @@ function Controller({}: any) {
     const getData = async () => {
         try {
           // Supondo que `localizacao_radares` seja uma lista de objetos
-          const rows = tabela_saldo_cargos;
+          const rows = saldo_cargos;
       
           // Mapeando os dados, adicionando o campo `kmh` se ele não existir
           const mappedRows: any = rows.map((item) => {
