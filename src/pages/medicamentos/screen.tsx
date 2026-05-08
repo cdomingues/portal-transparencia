b736accaf2315be6978991f475b3c097ab404f81
Carlos Domingues <carlosdomingues@mogidascruzes.sp.gov.br>
2026-05-08 15:02:19 -0300

    ajustes

--- src/pages/medicamentos/screen.tsx
+++ src/pages/medicamentos/screen.tsx
@@ -148,7 +148,7 @@ function Screen({ handler }: PropsInput) {
                         
                       />
         <Text fontSize={accessibility?.fonts?.regular} mb="10px">
-                        Última atualização: <strong>26/09/2025</strong>
+                        Última atualização: <strong>30/04/2025</strong>
                       </Text>
         <Table >
           <Thead>
